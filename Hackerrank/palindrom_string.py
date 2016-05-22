7925c15ffa5504a33709a1407036ea4af480f1b6
ScarfaceIII <massimo.lura@gmail.com>
2016-05-22 09:35:29 +0100

    Updated problem text.

--- Hackerrank/palindrom_string.py
+++ Hackerrank/palindrom_string.py
@@ -1,6 +1,19 @@
 '''
 
-function must return 1 if provided word is palindrom, 0 otherwise.
+Write a function which takes a string as a parameter and returns 1 
+if that word is a palindrome, and 0 otherwise. A palindrom is a word 
+which, when reversed, is the same as the original string e.g. "racecar".
+If no string is entered, the algorigthm should return 0.
+
+Examples
+Input: door
+Output: 0
+
+Input: abba
+Output: 1
+
+Input: ""
+Output: 0
 
 '''
 
